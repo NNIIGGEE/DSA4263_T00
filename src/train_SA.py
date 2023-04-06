7563037ecc04076cdfa9a255a3110b6e4f6d04ab
nigel chua <104724840+NNIIGGEE@users.noreply.github.com>
2023-04-06 22:50:07 +0800

    refactor_LDA

--- src/train_SA.py
+++ src/train_SA.py
@@ -1,8 +1,14 @@
 from sentiment_analysis.prep import preprocess
 from sentiment_analysis.train import evaluate, train_model
+from sentiment_analysis.topic_modelling import topic_modelling_LDA
 
+print("=========================CLEANING DATA=========================")
 data = preprocess.clean_data("../datasets/reviews.csv")
 print("=========================CLEANED DATA=========================")
+
+print("=========================PERFORMING LDA=========================")
+LDA_code = topic_modelling_LDA.full_LDA_topic_modelling(data)
+print("=========================LDA DONE=========================")
 
 print("=========================TRAINING MODEL=========================")
 train_model.run_training(data)
