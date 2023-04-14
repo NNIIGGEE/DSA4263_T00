fdd30dd67774604ec23bd832fabd172b9cff8fd0
nigel chua <104724840+NNIIGGEE@users.noreply.github.com>
2023-04-14 23:21:52 +0800

    push

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from flask import Flask, request, render_template
 from io import BytesIO
 import pandas as pd
-from src.sentiment_analysis.train import evaluate
+from src.sentiment_analysis.train import evaluate, train_model
 
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['GET','POST'])
-def handle_newfile():
+@app.route('/train', methods=['GET','POST'])
+def handle_trainfile():
+    print("function start")
     if request.method == "POST":
         file = request.files['file']
+        print(file)
+        print("html reuqest input")
+        predictions = train_model.run_lstm_training_full(BytesIO(file.read()))
+
+        return render_template('uploadfile_copy.html')
+
+    else:
+        return render_template('uploadfile_copy.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def handle_newfile():
+    print("function start")
+    if request.method == "POST":
+        file = request.files['file']
+        print("html reuqest input")
         predictions = evaluate.test_get_score(BytesIO(file.read()))
+        print("html evaluated")
         htmlcode = (predictions.to_html())
+        print("html open")
         textfile = open('templates/displayresult.html', 'w')
         textfile.write(htmlcode)
         textfile.close()
@@ -19,3 +37,6 @@ def handle_newfile():
 
     else:
         return render_template('uploadfile.html')
+
+if __name__ == "__main__":
+     app.run(debug=True ,port=5001,use_reloader=False) 
